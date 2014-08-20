ac8d1455c07fe1ce6920ae2ce8aec8c3e436af43
syntaxfish <syntaxfish@gmail.com>
2014-08-20 14:40:54 +0900

    환경설정 주석 제거

--- config.js
+++ config.js
@@ -3,20 +3,25 @@
 var config = {
     port: 3000,
     database: {
-        ACCOUNT: {
-            bucket: "beer-sample",
-            host: "stage.haru.io:8091",
-            // host: "localhost:8091",
-            // username: "account",
-            // password: "soma05baas"
-        },
-        TOKEN: {
-            bucket: "token",
-            host: "stage.haru.io:8091",
-            // user: "Administrator",
-            // password: "soma05baas"
-        }
-    }   
+        buckets:[
+            {
+                bucket: "account",
+                host: "localhost:8091",
+            },
+            {
+                bucket: "token",
+                host: "localhost:8091",
+            }
+        ]
+    },
+    write_server: {
+        host: "localhost", 
+        port: 8000
+    },
+    read_server: {
+        host: "localhost", 
+        port: 8000
+    }  
 };
-
+9
 module.exports = config;
