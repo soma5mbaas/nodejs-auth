a47243d2d0951994a53a8f34f11c45c4eceb7cff
syntaxfish <syntaxfish@gmail.com>
2014-08-25 18:41:25 +0900

    jwt-simple 모들 삭제

--- utils/util.js
+++ utils/util.js
@@ -1,23 +1,5 @@
-var jwt = require('jwt-simple');
 var md5 = require('MD5');
 var secret = 'xxx';
-
-/*
-// encode
-var token = jwt.encode(payload, secret);
-console.log(token);
-
-// decode
-var decoded = jwt.decode(token, secret);
-console.log(decoded);
-*/
-
-// use jwt-simple
-// exports.generateToken = function( json ) {
-// 	var token = jwt.encode(json, secret);
-
-// 	return token;
-// };
 
 // use md5
 exports.generateToken = function( json ) {
