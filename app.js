e0a59588bf9f782d2809b0a55218a54e3a352918
syntaxfish <syntaxfish@gmail.com>
2014-08-25 17:55:28 +0900

    uncaughtException 추가

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-
-
 var app = express();
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 app.use('/', routes);
+
+process.on('uncaughtException', function(error) {
+	console.log(error);
+});
 
 module.exports = app;
