af4fb44a1532c1e682a7793ad32b5027f7df5fc5
syntaxfish <syntaxfish@gmail.com>
2014-08-25 17:56:28 +0900

    token 발행 모듈 변경 jwt -> md5

--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,5 @@
 var jwt = require('jwt-simple');
+var md5 = require('MD5');
 var secret = 'xxx';
 
 /*
@@ -11,12 +12,20 @@ var decoded = jwt.decode(token, secret);
 console.log(decoded);
 */
 
+// use jwt-simple
+// exports.generateToken = function( json ) {
+// 	var token = jwt.encode(json, secret);
 
+// 	return token;
+// };
+
+// use md5
 exports.generateToken = function( json ) {
-	var token = jwt.encode(json, secret);
+	var token = md5(json.aid);
 
 	return token;
 };
+
 
 exports.generateAId = function( data ) {
 	return data.application_id +':'+ data.api.id+':'+ data.api.type + ':' + data.user_id;
